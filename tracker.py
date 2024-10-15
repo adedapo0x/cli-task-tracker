b412747086c377488fc29bd31061ee14a0f4214b
Adeyemi Adedapo <adedapo025@gmail.com>
2024-10-15 18:26:47 +0100

    Initial file reading implementation setup

--- tracker.py
+++ tracker.py
@@ -1,12 +1,23 @@
 import argparse
+import json
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('command')
+parser.add_argument('ID', nargs='?', default=1)
+parser.add_argument('task')
 
 args = parser.parse_args()
 
-command = str(args.command).lower()
+command = args.command.lower()
+id = args.ID
+task = args.task
+
+current_task = [command, id, task]
+
+with open('record.json', 'a') as record:
+    json.dump(current_task, record)
+
 
 
 
