66e06ed5e75b64528e346c8e62543b4a11202fee
Adeyemi Adedapo <adedapo025@gmail.com>
2024-10-20 10:00:14 +0100

    updated implementation of updateTask method

--- class_definition.py
+++ class_definition.py
@@ -12,7 +12,7 @@ class FileManager:
         line = ''
         try:
             with open(self.filename, 'r', encoding='utf-8') as file:
-                fileSize = os.path.getsize(file)
+                fileSize = os.path.getsize(self.filename)
 
                 if fileSize == 0:
                     return 0
@@ -26,7 +26,6 @@ class FileManager:
 
     def addToFile(self, data):
         lastId = self.getLastId()
-        print(lastId)
         inputted_data = {"id": lastId + 1,
                 "description": data,
                 "status": "toDo",
@@ -42,17 +41,51 @@ class FileManager:
         except Exception as e:
             print(f"Failed to append to file, {e}")
     
-    def updateTask(self, id):
-        with open(self.filename, 'a') as file:
-            if not os.path.exists(file):
+    def updateTask(self, inputID, update):
+        task = ""
+        if not os.path.exists(self.filename):
                 print("No task available in tracker!")
                 return
+        with open(self.filename, 'r+') as file:
             file_list = file.readlines()
             l = 0
-            r = len(file_list)
-            count = r // 2
+            r = len(file_list) - 1
+            task_index = -1
             while l <= r:
-                if file_list[l] > file_list[count]:
+                mid = (r + l) // 2
+                try:
+                    midVal = json.loads(file_list[mid])
+                    if midVal["id"] > inputID:
+                        r = mid - 1
+                    elif midVal["id"] < inputID:
+                        l = mid + 1
+                    elif midVal["id"] == inputID:
+                        task = json.loads(file_list[mid])
+                    else: 
+                        task_index = mid
+                        break
+                except Exception as e:
+                    print(f"Error decoding JSON at line {mid + 1}")
+                    return
+                if task_index == -1:
+                    print("Task ID not in tracker!")
+                    return 
+                task = json.loads(file_list[task_index])
+                task["description"] = update 
+
+                # prepare data to write back to file
+                file_list[task_index] = json.dumps(task) + '\n'
+
+                with open(self.filename, 'w') as file:
+                    file.writelines(file_list)
+                    
+            print(f"Task updated successfully! (ID: {inputID})")
+                    
+        
+            
+            
+                 
+
 
 
 
